6bc13d13da2a30c551958de444d6e01127b6dbfe
MahyarBHZ <mahyarbhz@gmail.com>
2024-10-10 00:10:31 +0330

    Adding api group

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/ping', function () {
+    return "Pong!";
+});
+
+Route::prefix('api')->group(function () {
+    Route::get('/', function () {
+        return "Hello World!";
+    });
 });
