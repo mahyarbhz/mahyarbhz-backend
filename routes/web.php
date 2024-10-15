2b1b392d27b2d9a5b221a3160cb8255126bd931a
MahyarBHZ <mahyarbhz@gmail.com>
2024-10-15 23:01:34 +0330

    Some basic configurations with sanctum

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,3 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/ping', function () {
-    return "Pong!";
-});
-
-Route::prefix('api')->group(function () {
-    Route::get('/', function () {
-        return "Hello World!";
-    });
-});
